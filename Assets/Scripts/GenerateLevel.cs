f4f3a4117c9542d81491c0a73282bf2a2bb1a3c5
Jacky K. Tse <21jackytse@gmail.com>
2022-06-03 15:02:11 -0700

    Added ground removal as object travels to save memory

--- Assets/Scripts/GenerateLevel.cs
+++ Assets/Scripts/GenerateLevel.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class GenerateLevel : MonoBehaviour
 {
     private Transform playerTransform;
+    private List<GameObject> activeGround;
 
     public GameObject[] groundPrefab;
     public float zPos = 10;
     public float groundLength = 25;
+    public float safeZone = 28;
     public int amountOfGround = 7;
 
     private void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        activeGround = new List<GameObject>();
         // I want to create 7 platforms
         for (int i = 0; i < amountOfGround; i++)
         {
@@ -24,18 +27,26 @@ public class GenerateLevel : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if(playerTransform.position.z > (zPos - amountOfGround * groundLength))
+        if(playerTransform.position.z - safeZone > (zPos - amountOfGround * groundLength))
         {
             SpawnGround();
+            DeleteGround();
         }
+    }
+
+    private void DeleteGround()
+    {
+        Destroy(activeGround[0]);
+        activeGround.RemoveAt(0);
     }
 
     private void SpawnGround(int prefabIndex = -1)
     {
         GameObject go;
-        go = Instantiate(groundPrefab[0]) as GameObject;
+        go = Instantiate(groundPrefab[0]);
         go.transform.SetParent(transform);
         go.transform.position = Vector3.forward * zPos;
         zPos += groundLength;
+        activeGround.Add(go);
     }
 }
